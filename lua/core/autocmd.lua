408ed2f97a27dc5569b8b5eaca2d9153f6b0a5f0
Jorge Galindo <jorge.galindo@wizeline.com>
2024-02-21 08:46:12 -0600

    add new plugins and remove autocmd

--- lua/core/autocmd.lua
+++ lua/core/autocmd.lua
@@ -1,8 +1,8 @@
-local api = vim.api
+-- local api = vim.api
 
 -- Remove all trailing whitespace on save
-local TrimWhitespaceGroup = api.nvim_create_augroup("TrimWhitespaceGroup", { clear = true })
-api.nvim_create_autocmd("BufWritePre", {
-  command = [[:%s/\s\+$//e]],
-  group = TrimWhitespaceGroup,
-})
+-- local TrimWhitespaceGroup = api.nvim_create_augroup("TrimWhitespaceGroup", { clear = true })
+-- api.nvim_create_autocmd("BufWritePre", {
+--   command = [[:%s/\s\+$//e]],
+--   group = TrimWhitespaceGroup,
+-- })
