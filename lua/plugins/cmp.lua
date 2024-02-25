f224f867eabeb0eec1454cf42bb239638e815eae
Jorge Galindo <jorge.galindo@wizeline.com>
2024-02-25 15:48:10 -0600

    updates cmp commands

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -35,11 +35,9 @@ return {
 					documentation = cmp.config.window.bordered(),
 				},
 				mapping = cmp.mapping.preset.insert({
-					["<C-b>"] = cmp.mapping.scroll_docs(-4),
-					["<C-f>"] = cmp.mapping.scroll_docs(4),
-					["<C-Space>"] = cmp.mapping.complete(),
-					["<C-e>"] = cmp.mapping.abort(),
-					["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+          ['<TAB>'] = cmp.mapping.select_next_item(cmp_select),
+          ['<S-TAB>'] = cmp.mapping.select_prev_item(cmp_select),
+          ['<CR>'] = cmp.mapping.confirm({ select = true }),
 				}),
 				sources = cmp.config.sources({
 					{ name = 'nvim_lsp' },
